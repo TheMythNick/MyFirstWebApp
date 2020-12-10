865e7381cbba8591724c98970aeaf8c23749eadd
unknown <nick.cashen1@hotmail.co.nz>
2020-12-10 21:06:34 +1300

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <img src='rudolph.jpg'></img>  
+        <img src='images/rudolph.jpg'></img>  
         <h1>
           Rudolph loose in Opaheke
         </h1>
