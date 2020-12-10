7c6f14a72d7c4b2eea43d33f22c7080951b45240
unknown <nick.cashen1@hotmail.co.nz>
2020-12-10 19:42:08 +1300

    commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src='rudolph.jpg'></img>  
+        <h1>
+          Rudolph loose in Opaheke
+        </h1>
+        <p>By Joshua.C</p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
